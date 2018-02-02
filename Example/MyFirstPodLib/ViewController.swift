0e4a9e776fbf505c9d9e84ffba51e15610ae164a
amit.rawat <amit.rawat@int.absas.net>
2018-02-02 14:47:14 +0530

    Initial commit

---
+++ Example/MyFirstPodLib/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyFirstPodLib
+//
+//  Created by amit.rawat on 02/02/2018.
+//  Copyright (c) 2018 amit.rawat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
